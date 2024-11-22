d9d7bb16f9f5890cc211deb0a7014a497f14ff2d
advaitaa-02 <advaitaabhardwaj@gmail.com>
2024-11-22 21:23:15 +0530

    created Player class

---
+++ Player.js
@@ -0,0 +1,18 @@
+class Player {
+    constructor(name, health, strength, attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    isAlive() {
+        return this.health > 0;
+    }
+
+    takeDamage(damage) {
+        this.health = Math.max(this.health - damage, 0);
+    }
+}
+
+module.exports = Player;
